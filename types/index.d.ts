d03a7ee1fbbe18f6463289b05970c5522b48cfeb
raghu <raghu.mudaliar@actonate.com>
2019-08-30 13:00:24 +0530

    Manually copied master changes to release-workflow

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,6 +8,7 @@ export interface SortStart {
   node: Element;
   index: number;
   collection: Offset;
+  isKeySorting: boolean;
 }
 
 export interface SortOver {
@@ -15,20 +16,22 @@ export interface SortOver {
   oldIndex: number;
   newIndex: number;
   collection: Offset;
+  isKeySorting: boolean;
 }
 
 export interface SortEnd {
   oldIndex: number;
   newIndex: number;
   collection: Offset;
+  isKeySorting: boolean;
 }
 
 export type SortEvent = React.MouseEvent<any> | React.TouchEvent<any>;
 
 export type SortEventWithTag = SortEvent & {
   target: {
-    tagName: string
-  }
+    tagName: string;
+  };
 };
 
 export type SortStartHandler = (sort: SortStart, event: SortEvent) => void;
@@ -39,7 +42,11 @@ export type SortEndHandler = (sort: SortEnd, event: SortEvent) => void;
 
 export type SortOverHandler = (sort: SortOver, event: SortEvent) => void;
 
-export type ContainerGetter = (element: React.ReactElement<any>) => HTMLElement | Promise<HTMLElement>;
+export type ContainerGetter = (
+  element: React.ReactElement<any>,
+) => HTMLElement | Promise<HTMLElement>;
+
+export type HelperContainerGetter = () => HTMLElement;
 
 export interface Dimensions {
   width: number;
@@ -51,10 +58,19 @@ export interface SortableContainerProps {
   lockAxis?: Axis;
   helperClass?: string;
   transitionDuration?: number;
+  keyboardSortingTransitionDuration?: number;
+  keyCodes?: {
+    lift?: number[];
+    drop?: number[];
+    cancel?: number[];
+    up?: number[];
+    down?: number[];
+  },
   pressDelay?: number;
   pressThreshold?: number;
   distance?: number;
   shouldCancelStart?: (event: SortEvent | SortEventWithTag) => boolean;
+  updateBeforeSortStart?: SortStartHandler;
   onSortStart?: SortStartHandler;
   onSortMove?: SortMoveHandler;
   onSortEnd?: SortEndHandler;
@@ -66,6 +82,7 @@ export interface SortableContainerProps {
   lockOffset?: Offset | [Offset, Offset];
   getContainer?: ContainerGetter;
   getHelperDimensions?: (sort: SortStart) => Dimensions;
+  helperContainer?: HTMLElement | HelperContainerGetter;
 }
 
 export interface SortableElementProps {
@@ -80,12 +97,28 @@ export interface Config {
 
 export type WrappedComponentFactory<P> = (props: P) => JSX.Element;
 
-export type WrappedComponent<P> = React.ComponentClass<P> | WrappedComponentFactory<P>;
+export type WrappedComponent<P> =
+  | React.ComponentClass<P>
+  | React.SFC<P>
+  | WrappedComponentFactory<P>;
 
-export function SortableContainer<P>(wrappedComponent: WrappedComponent<P>, config?: Config): React.ComponentClass<P & SortableContainerProps>;
+export function SortableContainer<P>(
+  wrappedComponent: WrappedComponent<P>,
+  config?: Config,
+): React.ComponentClass<P & SortableContainerProps>;
 
-export function SortableElement<P>(wrappedComponent: WrappedComponent<P>, config?: Config): React.ComponentClass<P & SortableElementProps>;
+export function SortableElement<P>(
+  wrappedComponent: WrappedComponent<P>,
+  config?: Config,
+): React.ComponentClass<P & SortableElementProps>;
 
-export function SortableHandle<P>(wrappedComponent: WrappedComponent<P>, config?: Config): React.ComponentClass<P>;
+export function SortableHandle<P>(
+  wrappedComponent: WrappedComponent<P>,
+  config?: Config,
+): React.ComponentClass<P>;
 
-export function arrayMove<T>(collection: T[], previousIndex: number, newIndex: number): T[];
+export function arrayMove<T>(
+  collection: T[],
+  previousIndex: number,
+  newIndex: number,
+): T[];
